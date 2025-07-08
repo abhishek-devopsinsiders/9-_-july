edb1a91c6a74ec9842a068183615557466c155a9
abhishek prakash <abhishekmouar@gmail.com>
2025-07-09 00:17:26 +0530

    my code

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource azurerm_resource_group "rg-block" {
+  name     = "rg1"
+  location = "West Europe"
+}
+
+resource azurerm_storage_account "sa-block" {
+  name                     = "stgaccount1"
+  resource_group_name      = azurerm_resource_group.rg-block.name
+  location                 = azurerm_resource_group.rg-block.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
